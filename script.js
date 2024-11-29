001bb661bb3135c3da673dcef0c91d4b10bc0554
satyabrata11x <satyabrata11x@gmail.com>
2024-11-29 12:14:49 +0530

    Update

---
+++ script.js
@@ -0,0 +1,59 @@
+const accesskey = "9fiMOQl1bhnJ_XX_Hbh_VJvqThQ9y3hK8ui9hszsDEQ";
+
+const searchForm = document.getElementById("search-from");
+const searchBox = document.getElementById("search-box");
+const searchResult = document.getElementById("search-result");
+const showMoreBtn = document.getElementById("show-more-btn");
+
+
+
+async function searchImages() {
+    keyword = searchBox.value;
+    const url = `https://api.unsplash.com/search/photos?page=1&query=${keyword}&client_id=${accesskey}&per_page=12`;
+
+    const response = await fetch(url);
+    const data = await response.json();
+
+    if(page == 1){
+        searchResult.innerHTML = "";
+    }
+
+   const results = data.results;
+
+   results.map((result) =>{
+      const image = document.createElement("img");
+      image.src = result.urls.small;
+      const imageLink =  document.createElement("a");
+      imageLink.href = result.links.html;
+      imageLink.target = "_blank";
+
+      imageLink.appendChild(image);
+      searchResult.appendChild(imageLink);
+   })
+   showMoreBtn.style.display = "block";
+}
+
+
+searchForm.addEventListener("submit" , (e) => {
+    e.preventDefault();
+    page = 1;
+    searchImages();
+})
+
+showMoreBtn.addEventListener("click" , ()=>{
+     page++;
+     searchImages();
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
