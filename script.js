7b66a7e034ab1670288829ad2dd10f75ef313795
satyabrata11x <satyabrata11x@gmail.com>
2024-12-02 16:38:50 +0530

    update

--- script.js
+++ script.js
@@ -4,12 +4,12 @@ const searchForm = document.getElementById("search-from");
 const searchBox = document.getElementById("search-box");
 const searchResult = document.getElementById("search-result");
 const showMoreBtn = document.getElementById("show-more-btn");
-
+let page = 1;
 
 
 async function searchImages() {
     keyword = searchBox.value;
-    const url = `https://api.unsplash.com/search/photos?page=1&query=${keyword}&client_id=${accesskey}&per_page=12`;
+    const url = `https://api.unsplash.com/search/photos?page=${page}&query=${keyword}&client_id=${accesskey}&per_page=12`;
 
     const response = await fetch(url);
     const data = await response.json();
